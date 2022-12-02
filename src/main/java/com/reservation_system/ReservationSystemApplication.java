5495458b69c9e8ad5fa02e22241cb81ba13b351f
Adam Henry <adam.henry@gmail.com>
2022-12-02 13:40:25 -0600

    support Amenity capacity

--- src/main/java/com/reservation_system/ReservationSystemApplication.java
+++ src/main/java/com/reservation_system/ReservationSystemApplication.java
@@ -1,8 +1,10 @@
 package com.reservation_system;
 
 import com.reservation_system.model.AmenityType;
+import com.reservation_system.model.Capacity;
 import com.reservation_system.model.Reservation;
 import com.reservation_system.model.User;
+import com.reservation_system.repos.CapacityRepository;
 import com.reservation_system.repos.ReservationRepository;
 import com.reservation_system.repos.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -17,17 +19,28 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @SpringBootApplication
 public class ReservationSystemApplication {
+
+    private Map<AmenityType, Integer> initialCapacities =
+            new HashMap<>() {
+                {
+                    put(AmenityType.GYM, 20);
+                    put(AmenityType.POOL, 4);
+                    put(AmenityType.SAUNA, 1);
+                }
+            };
 
     public static void main(final String[] args) {
         SpringApplication.run(ReservationSystemApplication.class, args);
     }
 
     @Bean
-    public CommandLineRunner loadData(UserRepository userRepository, ReservationRepository reservationRepository) {
+    public CommandLineRunner loadData(UserRepository userRepository, ReservationRepository reservationRepository, CapacityRepository capacityRepository) {
         return (args) -> {
             User user = userRepository.save(new User("Adam Henry", "ahenry", bCryptPasswordEncoder().encode("1234")));
             User user2 = userRepository.save(new User("John Doe", "jd", bCryptPasswordEncoder().encode("1234")));
@@ -43,6 +56,10 @@ public class ReservationSystemApplication {
                     .build();
 
             reservationRepository.save(reservation);
+
+            for (AmenityType amenityType : initialCapacities.keySet()) {
+                capacityRepository.save(new Capacity(amenityType, initialCapacities.get(amenityType)));
+            }
         };
     }
 
